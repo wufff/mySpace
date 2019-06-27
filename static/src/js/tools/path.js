9bf5a6008a4a7fb3880cc15ae96cb273766d6ade
wufff <wfqjnzb@sina.com>
2019-06-27 16:55:00 +0800

    qd

--- static/src/js/tools/path.js
+++ static/src/js/tools/path.js
@@ -1,6 +1,6 @@
 define(function(){
      return {
          localhostImg:"/mySpace/img",
-         imgPath:"http://images.dev.dodoedu.com/rjsPage/frontend/base/global/images"
+         static:"http://images.dev.dodoedu.com/rjsPage/frontend/base/global/images",
      }
 })
